3335da87c23c9ce9bf84eab2bff62275bab7836c
AlexSteeves <48775637+AlexSteeves@users.noreply.github.com>
2024-11-24 18:18:10 -0500

    feat: grid-updates

--- app/components/Experience/index.jsx
+++ app/components/Experience/index.jsx
@@ -1,17 +1,20 @@
 import { experience } from "./ExperienceData";
-import { useInView, motion } from "framer-motion";
-import { useRef } from "react";
-import Slider from '../SharedComponents/Slider';
+import { motion } from "framer-motion";
+import Slider from "../SharedComponents/Slider";
 export default function index() {
   return (
     <>
       <Slider text="Experience" direction="1" />
       <div
         id="experience"
-        className="w-[100%] pb-10 text-text flex flex-col mt-20 justify-center items-center gap-16"
+        className="max-w-[1400px] mx-auto grid p-4 gap-4 my-[5vh] lg:my-[10vh]"
+        style={{
+          gridTemplateColumns: "repeat(auto-fill, minmax(450px, 1fr))",
+          gridAutoRows: "1fr",
+        }}
       >
-        {experience.map((job, i) => (
-          <Experience key={i} experienceData={job} />
+        {experience.map((job, index) => (
+          <Experience key={index} experienceData={job} />
         ))}
       </div>
     </>
@@ -19,85 +22,43 @@ export default function index() {
 }
 
 function Experience({ experienceData }) {
-  const body = useRef(null);
-  const isInView = useInView(body, { once: true, margin: "-50%" });
-
-  const animation = {
-    initial: { y: "100%" },
-    enter: (i) => ({
-      y: "0",
-      transition: {
-        duration: 0.75,
-        ease: [0.33, 1, 0.68, 1],
-        delay: 0.075 * i,
-      },
-    }),
-  };
   return (
-    <div ref={body} className="flex flex-col gap-4 w-[80%]">
-      <div className = "w-full md:max-w-min bg-[#f4faf5] text-[#13481F] p-10 rounded-2xl">
-      <div className="overflow-hidden">
-        <motion.h3
-          variants={animation}
-          custom={1}
-          animate={isInView ? "enter" : "initial"}
-          className=" font-bold uppercase leading-[0.90] text wrap md:whitespace-nowrap"
+    <motion.div
+      whileHover={{ y: -10 }}
+      transition={{ duration: 0.2 }}
+      className="h-full"
+    >
+      <div className="flex flex-col gap-4 w-full h-full bg-[#f4faf5] text-[#13481F] p-4 md:p-8 rounded-xl shadow-lg hover:shadow-xl hover:bg-white">
+        <div className="flex flex-col">
+          <span className="font-[400] uppercase tracking-[0.05em]">
+            {experienceData.jobtitle}
+          </span>
+
+          <span className="font-[700] font-serif text-[1.33rem] uppercase">
+            {experienceData.company}
+          </span>
+        </div>
+
+        <span className="font-[300] text-balance">{experienceData.body}</span>
+
+        <div className="flex flex-col gap-2">
+          <p className="font-semibold">TECHNOLOGIES USED:</p>
+          <ul
+            className="grid list-disc list-inside gap-1"
             style={{
-              fontSize: "calc(16px + 3vw)",
+              display: "grid",
+              gap: "0.5rem",
+              gridTemplateColumns: "repeat(auto-fill, minmax(170px, 1fr))",
             }}
-        >
-          {experienceData.jobtitle}
-        </motion.h3>
-      </div>
-
-      <div className="overflow-hidden">
-        <motion.h2
-          variants={animation}
-          custom={2}
-          animate={isInView ? "enter" : "initial"}
-          className=" font-normal leading-[1.1] "
-            style={{ fontSize: "calc(16px + 1vw)" }}
-        >
-          {experienceData.company}
-        </motion.h2>
-      </div>
-
-      <div className="overflow-hidden">
-        <motion.p
-          variants={animation}
-          custom={3}
-          animate={isInView ? "enter" : "initial"}
-          className="font-light pt-5 text-balance leading-[1.1]"
-          style={{ fontSize: "calc(16px + 0.8vw)" }}
-        >
-          {experienceData.body}
-        </motion.p>
-      </div>
-
-      <div className="overflow-hidden">
-        <motion.div
-          variants={animation}
-          custom={3}
-          animate={isInView ? "enter" : "initial"}
-          className="w-fit flex flex-col gap-2"
-        >
-          <p className="font-semibold pt-5" style={{ fontSize: "calc(16px + 1vw)" }}>
-            TECHNOLOGIES USED:
-          </p>
-          <ul className="grid grid-cols-2 list-disc list-inside gap-x-4"
-              style={{ fontSize: "calc(16px + 0.5vw)" }}>
+          >
             {experienceData.skills.map((skill) => (
-              <li
-                className="font-regular whitespace-nowrap"
-                key={skill}
-              >
+              <li className="font-normal whitespace-nowrap" key={skill}>
                 {skill}
               </li>
             ))}
           </ul>
-        </motion.div>
+        </div>
       </div>
-      </div>
-    </div>
+    </motion.div>
   );
 }
--- app/components/Projects/projectData.js
+++ app/components/Projects/projectData.js
@@ -1,82 +1,92 @@
-
 export const projects = [
-    {
-  
-      
-      title:"Nourish Meal Generator",
-      description:"Nourish is a meal plan generator that allows users to create customized meal plans using GPT3.5-turbo.",
-  
-      skills: ["Framer Motion","Next.js",  "TailwindCSS", "Axios", "GPT-3.5-turbo", "Vercel", "Supabase"],
-      link: "https://github.com/AlexSteeves/Nourish_v2.0",
-      backgroundColor: "#47145b",
-        textColor: "#f3f0f5",
-    },
-    
-    {
- 
-      title: "Personal Portfolio",
-      description: "Designed and built this porftolio to display skills and link all of my socials. Hosted with Vercel.",
-   
-      skills: ["Framer Motion","Next.js", "GSAP",  "Locomotive", "TailwindCSS", "Vercel"],
-      link: "https://github.com/AlexSteeves/Portfolio_v2.0.1",
-      backgroundColor: "#47145b",
-      textColor: "#f3f0f5",
-    },
-    
-    {
-      
-      title: "Clarify Smart Mirror",
-      description: "Lead the development of the Phone application, using ReactNative and TypeScript to develop and ExpoGo to host.",
-      skills: ["ReactNative", "ExpoGo", "TypeScript"],
-      link: "https://github.com/uoClarify/clarify-phone-app",
-      backgroundColor: "#47145b",
-      textColor: "#f3f0f5",
-    },
-    {
-    
-      title: "Hotel Palace",
-      description: "Full Stack hotel booking app which allows customers to see room visibility and booking, and make reservations.",
-      skills: ["React.js", "PostgeSQL","Bootstrap", "Node.js"],
-      link: "https://github.com/MichiasShiferaw/database-proj",
-      backgroundColor: "#47145b",
-      textColor: "#f3f0f5",
-    },
-    {
-    
-      title: "Large Language Model",
-      description: "Large Language Model I built following Andrej Karpathy that trains a model on text data. ",
-      skills: ["Python", "PyTorch", "Jupyter Notebook"],
-      link: "https://github.com/AlexSteeves/Basic-GPTLanguageModel",
-      backgroundColor: "#47145b",
-      textColor: "#f3f0f5",
-    },
-  
-  ];
+  {
+    title: "Nourish Meal Generator",
+    description:
+      "Nourish is a meal plan generator that allows users to create customized meal plans using GPT3.5-turbo.",
+
+    skills: [
+      "Framer Motion",
+      "Next.js",
+      "Axios",
+      "GPT-3.5-turbo",
+      "Vercel",
+      "Supabase",
+    ],
+    link: "https://github.com/AlexSteeves/Nourish_v2.0",
+    backgroundColor: "#47145b",
+    textColor: "#f3f0f5",
+  },
+
+  {
+    title: "Personal Portfolio",
+    description:
+      "Designed and built this porftolio to display skills and link all of my socials. Hosted with Vercel.",
+
+    skills: [
+      "Framer Motion",
+      "Next.js",
+      "GSAP",
+      "Locomotive",
+      "TailwindCSS",
+      "Vercel",
+    ],
+    link: "https://github.com/AlexSteeves/Portfolio_v2.0.1",
+    backgroundColor: "#47145b",
+    textColor: "#f3f0f5",
+  },
+
+  {
+    title: "Clarify Smart Mirror",
+    description:
+      "Lead the development of the Phone application, using ReactNative and TypeScript to develop and ExpoGo to host.",
+    skills: [
+      "React Native",
+      "ExpoGo",
+      "TypeScript",
+      "Electron JS",
+      "Python",
+      "NLP",
+    ],
+    link: "https://github.com/uoClarify/clarify-phone-app",
+    backgroundColor: "#47145b",
+    textColor: "#f3f0f5",
+  },
+
+  {
+    title: "Large Language Model",
+    description:
+      "Large Language Model I built following Andrej Karpathy that trains a model on text data. ",
+    skills: ["Python", "PyTorch", "Jupyter Notebook"],
+    link: "https://github.com/AlexSteeves/Basic-GPTLanguageModel",
+    backgroundColor: "#47145b",
+    textColor: "#f3f0f5",
+  },
+];
 
 export const experience = [
-    {
-      jobtitle: "Data Analyst",
-     
-      company: "Canada Revenue Agency",
-      body: "I produced detailed analytics reports using Cognos BI, improving case management efficiency. I helped find a 50% data discrepancy within the legacy and new database management systems.",
-      skills: "SAS Viya, Cognos BI, Excel",
-      time: "Summer 2023",
-    },
-    {
-      jobtitle: "Software Developer",
-    
-      company: "Canada Border Services Agency",
-      body: "I developed an automated email filing system, cutting task completion time by 80%, and built an interactive database system that improved officers operational efficiency through real-time data access.",
-      skills: "Java, mySQL, HTML, CSS, Java Swing, ApachePOI, Eclipse",
-      time: "Winter 2022 - Winter 2023",
-    },
-  
-    {
-      jobtitle: "Data Analyst",
-     
-      company: "Transport Canada",
-      body: "I created a Visual Basic application suite for risk assessment, enhancing rail safety and efficiency. This automation resulted in a 30% decrease in the manual review time by officers, streamlining the risk analysis process.",
-      skills: "Visual Basic, Excel",
-      time: "Summer 2021",
-    },
-  ];
+  {
+    jobtitle: "Data Analyst",
+
+    company: "Canada Revenue Agency",
+    body: "I produced detailed analytics reports using Cognos BI, improving case management efficiency. I helped find a 50% data discrepancy within the legacy and new database management systems.",
+    skills: "SAS Viya, Cognos BI, Excel",
+    time: "Summer 2023",
+  },
+  {
+    jobtitle: "Software Developer",
+
+    company: "Canada Border Services Agency",
+    body: "I developed an automated email filing system, cutting task completion time by 80%, and built an interactive database system that improved officers operational efficiency through real-time data access.",
+    skills: "Java, mySQL, HTML, CSS, Java Swing, ApachePOI, Eclipse",
+    time: "Winter 2022 - Winter 2023",
+  },
+
+  {
+    jobtitle: "Data Analyst",
+
+    company: "Transport Canada",
+    body: "I created a Visual Basic application suite for risk assessment, enhancing rail safety and efficiency. This automation resulted in a 30% decrease in the manual review time by officers, streamlining the risk analysis process.",
+    skills: "Visual Basic, Excel",
+    time: "Summer 2021",
+  },
+];
