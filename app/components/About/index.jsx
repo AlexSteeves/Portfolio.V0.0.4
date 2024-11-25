b30dce44865cf7a5efbbcc083f18c61ade61ab76
AlexSteeves <48775637+AlexSteeves@users.noreply.github.com>
2024-11-25 16:59:26 -0500

    eslint error

--- app/components/About/index.jsx
+++ app/components/About/index.jsx
@@ -1,14 +1,4 @@
 "use client";
-import { useInView, motion } from "framer-motion";
-import { useRef } from "react";
-const aboutText = [
-  "I love building responsive and useable applications,",
-  "with a focus on scalability.",
-  "I have a strong passion for programming and love to learn new things.",
-];
-
-const aboutText2 =
-  "I love building responsive and useable applications with a focus on scalability. I have a strong passion for programming and love to learn new things.";
 
 export default function AboutMe() {
   return (
@@ -34,44 +24,11 @@ export default function AboutMe() {
             With a degree in Computer Engineering, work experience with Java,
             SQL as well as data science, and many projects built using
             React/Next.JS, I am interested in just about every part of Software
-            whether that's low/high level programming, or frontend/backend.
+            whether that&rsquo;s low/high level programming, or
+            frontend/backend.
           </h3>
         </div>
       </div>
     </div>
   );
 }
-const MaskText = ({ text, custom }) => {
-  const ref = useRef(null);
-  const inView = useInView(ref, { threshold: 1, triggerOnce: false });
-
-  const animation = {
-    initial: { y: "100%" },
-    enter: {
-      y: "0",
-      transition: {
-        duration: 0.75,
-        ease: [0.33, 1, 0.68, 1],
-        delay: 0.075 * custom,
-      },
-    },
-  };
-
-  {
-    /* <span className="leading-[1.1] tracking-[-0.03em] font-[300] text-balance text-[1.5rem] lg:text-[2rem]">
-          
-          </span> */
-  }
-  return (
-    <div className="overflow-hidden" ref={ref}>
-      <motion.span
-        variants={animation}
-        initial="initial"
-        animate={inView ? "enter" : "initial"}
-        className="leading-[1.1] tracking-[-0.03em] font-[300] text-balance text-[1.5rem] lg:text-[2rem]"
-      >
-        {text}
-      </motion.span>
-    </div>
-  );
-};
